c857f1a414e3ebc9ff00f93cbfb96bc884584910
Jordan <jwrudge100@gmail.com>
2026-03-20 00:02:51 +0000

    update nav to have food and planning hub, along with account button

---
+++ src/MealPlanner.Web/Pages/Food/Index.cshtml.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MealPlanner.Web.Pages.Food;
+
+public class IndexModel : PageModel
+{
+    public void OnGet() { }
+}
---
+++ src/MealPlanner.Web/Pages/Planning/Index.cshtml.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MealPlanner.Web.Pages.Planning;
+
+public class IndexModel : PageModel
+{
+    public void OnGet() { }
+}
